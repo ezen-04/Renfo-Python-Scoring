acbc1ab2135870e14cd3bd869be333ef05b2f729
Ezéchiel N'DRI <125416353+ezen-04@users.noreply.github.com>
2026-05-18 09:13:21 +0000

    Initial commit

--- data.py
+++ data.py
@@ -1,23 +1,27 @@
+# Importaion des différentes bibliothèques
 import yfinance as yf
 import pandas as pd
 from sqlalchemy import create_engine, text
 
+# Dictionnaire des tickers et leurs tables correspondantes
 tickers = {
-    "AAPL": "prices_AAPL",
-    "MSFT": "prices_MSFT",
-    "BP.L": "prices_BP_L",
-    "HSBA.L": "prices_HSBA_L",
-    "7203.T": "prices_7203_T",
-    "6758.T": "prices_6758_T",
-    "TTE.PA": "prices_TTE_PA",
-    "MC.PA": "prices_MC_PA",
-    "SIE.DE": "prices_SIE_DE",
-    "BMW.DE": "prices_BMW_DE"
+    "AAPL": "prices_AAPL", # Apple Inc.
+    "MSFT": "prices_MSFT", # Microsoft Corporation
+    "BP.L": "prices_BP_L", # BP plc
+    "HSBA.L": "prices_HSBA_L", # HSBC Holdings plc
+    "7203.T": "prices_7203_T", # Toyota Motor Corporation
+    "6758.T": "prices_6758_T", # Sony Corporation
+    "TTE.PA": "prices_TTE_PA", # TotalEnergies SE
+    "MC.PA": "prices_MC_PA", # LVMH Moët Hennessy Louis Vuitton SE
+    "SIE.DE": "prices_SIE_DE", # Siemens AG
+    "BMW.DE": "prices_BMW_DE" # Bayerische Motoren Werke AG
 }
 
+# Connection à la base de données MySQL
 engine = create_engine("mysql+pymysql://root:@localhost/market_db")
 
-# Création des tables
+
+# Création des tables pour chaque ticker dans la base de données
 with engine.connect() as conn:
     for table_name in tickers.values():
         create_table_sql = f"""
@@ -35,11 +39,11 @@ with engine.connect() as conn:
         conn.execute(text(create_table_sql))
         print(f"✅ Table {table_name} créée.")
 
-# Téléchargement et insertion
+# Téléchargement et insertion des données pour chaque ticker
 for ticker, table_name in tickers.items():
     print(f"📥 Téléchargement de {ticker}...")
     stock = yf.Ticker(ticker)
-    data = stock.history(start="2014-01-01", end="2024-12-31")
+    data = stock.history(start="2010-01-01", end="2026-04-30")
     
     if data.empty:
         print(f"⚠️ Pas de données pour {ticker}, ignoré.")
